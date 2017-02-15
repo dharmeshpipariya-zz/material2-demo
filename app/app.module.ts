3475109d4d6839ed9a786b471c1f568389b6c999
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-02-15 11:32:55 +0530

    update md2 and material

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
 import { Md2Module } from 'md2';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     Md2Module.forRoot(),
   ],
   declarations: [AppComponent],
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,51 +1,29 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
-(function (global) {
-  // map tells the System loader where to look for things
-  var map = {
-    'app': 'app', // 'dist',
-    '@angular': 'node_modules/@angular',
-    'md2': 'node_modules/md2',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs': 'node_modules/rxjs'
-  };
-  // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-    'app': { main: 'main.js', defaultExtension: 'js' },
-    'md2': { format: 'cjs', main: 'md2.umd.js' },
-    'rxjs': { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-  };
+// Apply the CLI SystemJS configuration.
+System.config({
+  map: {
+    'rxjs': 'node_modules/rxjs',
+    'main': 'main.js',
 
-  var ngPackageNames = [
-  'common',
-  'compiler',
-  'core',
-  'forms',
-  'http',
-  'platform-browser',
-  'platform-browser-dynamic',
-  'router',
-  'router-deprecated',
-  'upgrade',
-  ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    // Angular specific mappings.
+    '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
+    '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
+    '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
+    '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
+    '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
+    '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic':
+      'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/material': 'node_modules/@angular/material/bundles/material.umd.js',
+    'md2': 'node_modules/md2/bundles/md2.umd.js'
+  },
+  packages: {
+    // Thirdparty barrels.
+    'rxjs': { main: 'index' },
+    // Set the default extension for the root package, because otherwise the demo-app can't
+    // be built within the production mode. Due to missing file extensions.
+    '.': {
+      defaultExtension: 'js'
+    }
   }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/' + pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  var config = {
-    map: map,
-    packages: packages
-  };
-  System.config(config);
-})(this);
+});
